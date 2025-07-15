04f46b5f2eb19cd90e052557e26869dc344376e8
abbv75 <bore.younous59@gmail.com>
2025-07-15 20:08:55 +0000

    `Removed src/constant/index.js and added new types in src/types/index.ts`

--- src/constant/index.js
+++
@@ -1,2 +0,0 @@
-export { default as PAGE_PATH } from "./pagePath";
-export { default as IMAGES } from "./image";
--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,30 @@ export type LOADING_STATE_T = "En cours de chargement."
   | "Chargememnt reussi"
   | "En attente"
   | null;
+
+export type ROLE_T = {
+  id: string;
+  nom: string;
+  description: string;
+};
+
+export type CONTACT_T = {
+  id: string;
+  telephone: string;
+  email?: string;
+  adresse?: string;
+  whatsapp?: string;
+};
+
+export type USER_T = {
+  id: string;
+  nomComplet: string;
+  login: string;
+  id_role: string;
+  id_contact: string;
+  idCOD?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  role?: ROLE_T;
+  contact?: CONTACT_T;
+};
