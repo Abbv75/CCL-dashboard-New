083ab908af0aad5c9853a6cd7c6b1a876f9febf0
abbv75 <bore.younous59@gmail.com>
2025-07-16 13:41:37 +0000

    Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,22 @@
-import { Card } from '@mui/joy'
-import React from 'react'
+import { Avatar, ButtonGroup, Card, IconButton, Stack, Typography } from '@mui/joy'
+import { CardMedia } from '@mui/material'
+import { IMAGES } from '../../constant'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLock, faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
     return (
-        <Card></Card>
+        <Card sx={{ p: 1, borderRadius: 0 }} >
+            <Stack direction={"row"} justifyContent={"space-between"} >
+                <Typography
+                    startDecorator={<Avatar children={<CardMedia component={"img"} src={IMAGES.logo} />} />}
+                >CCL</Typography>
+                <ButtonGroup variant='plain' >
+                    <IconButton children={<FontAwesomeIcon icon={faUserAstronaut} />} />
+                    <IconButton children={<FontAwesomeIcon icon={faLock} />} />
+                </ButtonGroup>
+            </Stack>
+        </Card>
     )
 }
 
