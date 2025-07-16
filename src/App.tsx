1ce60657e1d384b7399a07b9f4b1bf5153dc14ac
abbv75 <bore.younous59@gmail.com>
2025-07-16 12:59:23 +0000

    loadingUserState == null

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,32 @@ import { BrowserRouter } from 'react-router-dom'
 import Router from './Routes'
 import { AppContext } from './providers/AppContext'
 import { ToastContainer } from 'react-toastify'
-import { useState } from 'react'
-import { USER_T } from './types'
+import React, { useCallback, useEffect, useState } from 'react'
+import { LOADING_STATE_T, USER_T } from './types'
+import { Skeleton, Typography } from '@mui/joy'
 
 const App = () => {
   const [currentUser, setcurrentUser] = useState(undefined as undefined | USER_T);
+  const [loadingUserState, setloadingUserState] = useState("En cours de chargement." as LOADING_STATE_T);
+
+  const loadUser = useCallback(() => {
+    try {
+      setloadingUserState("En cours de chargement.");
+
+      const resTmp = localStorage.getItem('currentUser');
+      const res = resTmp ? JSON.parse(resTmp) as USER_T : undefined;
+
+      setcurrentUser(res);
+
+      return res;
+    } catch (error) { } finally {
+      setloadingUserState(null);
+    }
+  }, [])
+
+  useEffect(() => {
+    loadUser();
+  }, [])
 
   return (
     <AppContext.Provider value={{
@@ -15,7 +36,10 @@ const App = () => {
       <BrowserRouter>
         <ToastContainer />
 
-        <Router />
+        <Skeleton loading={!!loadingUserState} >
+          <Router />
+        </Skeleton>
+
       </BrowserRouter>
     </AppContext.Provider>
   )
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -3,22 +3,22 @@ import { PAGE_PATH } from '../constant';
 import { useContext } from 'react';
 import { AppContext } from '../providers/AppContext';
 import Connexion from '../pages/Connexion';
+import Dashboard from '../pages/Dashboard';
 
 const Router = () => {
   const { currentUser } = useContext(AppContext);
 
   return (
     <Routes>
-      <Route path="/" element={currentUser ? <>accueil</> : <Connexion />} />
+      <Route path="/*" element={currentUser ? <Dashboard /> : <Connexion />} />
 
-      {PAGE_PATH.filter(({ isPublic }) => isPublic || currentUser)
-        .map((path, index) => (
-          <Route
-            key={index}
-            path={path.href}
-            Component={path.component}
-          />
-        ))}
+      {/* {PAGE_PATH.filter(({ isPublic }) => isPublic || currentUser).map((path, index) => (
+        <Route
+          key={index}
+          path={path.href}
+          Component={path.component}
+        />
+      ))} */}
     </Routes>
   );
 };
