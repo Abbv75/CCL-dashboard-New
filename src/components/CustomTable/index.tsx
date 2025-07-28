d62afd1a6b0a7f9eaa73a7d7c782b3a1eb04a79b
abbv75 <bore.younous59@gmail.com>
2025-07-28 15:31:24 +0000

    `feat: Add search functionality to CustomTable component`

--- src/components/CustomTable/index.tsx
+++ src/components/CustomTable/index.tsx
@@ -1,7 +1,51 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { faSearch, faSearchMinus, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Avatar, Card, Input, Sheet, Stack } from '@mui/joy'
+import { Collapse, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { useMemo, useState } from 'react'
 
 const CustomTable = ({ theadCells, data }: { theadCells: string[], data: any[][] }) => {
+    const [showSearchBar, setshowSearchBar] = useState(false);
+    const [search, setsearch] = useState(undefined as undefined | string);
+
+    const filteredData = useMemo(() => (data.filter(value => {
+        if (!search) return true;
+
+        return value.some(element => element?.toString()?.toLowerCase()?.includes(search.toLowerCase()))
+    })), [data, search])
+
+
     return (
+        <Sheet>
+            <Stack
+                direction={'row'}
+                gap={1}
+                alignItems={'center'}
+                component={Card}
+                p={0.5}
+            >
+                <Collapse in={showSearchBar} orientation='horizontal' >
+                    <Input
+                        placeholder='Rechercher ...'
+                        onChange={({ target }) => setsearch(target.value)}
+                        value={search}
+                        endDecorator={<FontAwesomeIcon
+                            icon={faTimesCircle}
+                            color='red'
+                            onClick={() => setsearch('')}
+                            style={{ cursor: 'pointer' }}
+                        />}
+                    />
+                </Collapse>
+
+                <Avatar
+                    color='primary'
+                    children={<FontAwesomeIcon icon={showSearchBar ? faSearchMinus : faSearch} />}
+                    onClick={() => setshowSearchBar(!showSearchBar)}
+                    sx={{ cursor: 'pointer' }}
+                />
+            </Stack>
+
             <TableContainer >
                 <Table>
                     <TableHead>
@@ -13,7 +57,7 @@ const CustomTable = ({ theadCells, data }: { theadCells: string[], data: any[][]
                     </TableHead>
 
                     <TableBody>
-                        {data.map((value, CustomTable) => (
+                        {filteredData.map((value, CustomTable) => (
                             <TableRow key={CustomTable} >
                                 {value.map((valueTmp, CustomTable) => (
                                     <TableCell key={CustomTable} >{valueTmp}</TableCell>
@@ -23,6 +67,7 @@ const CustomTable = ({ theadCells, data }: { theadCells: string[], data: any[][]
                     </TableBody>
                 </Table>
             </TableContainer>
+        </Sheet>
     )
 }
 
